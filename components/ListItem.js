b21b9a15300eb55ec01e37363787a2b6eeb4558b
ZIANG ZHAO <ZiangZhao1227@users.noreply.github.com>
2021-01-11 15:43:03 +0200

    some styles added

---
+++ components/ListItem.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+const ListItem = (props) => {
+  return (
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.imagebox}>
+        <Image
+          style={styles.image}
+          source={{uri: props.singleMedia.thumbnails.w160}}
+        />
+      </View>
+      <View style={styles.textbox}>
+        <Text style={styles.listTitle}>{props.singleMedia.title}</Text>
+        <Text>{props.singleMedia.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: '#eee',
+    borderRadius: 6,
+  },
+  listTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 15,
+  },
+  image: {
+    flex: 1,
+    borderRadius: 6,
+  },
+  textbox: {
+    flex: 2,
+    padding: 10,
+  },
+  imagebox: {
+    flex: 1,
+  },
+});
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
+};
+
+export default ListItem;
