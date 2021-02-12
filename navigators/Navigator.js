e3813163b597c39b4317fa41434eb51026618e06
ZIANG ZHAO <ZiangZhao1227@users.noreply.github.com>
2021-02-12 13:04:26 +0200

    task b done

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
 import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -12,6 +14,8 @@ import Login from '../views/Login';
 import {MainContext} from '../contexts/MainContext';
 import {Icon} from 'react-native-elements';
 import Upload from '../views/Upload';
+import MyFiles from '../views/MyFiles';
+import Modify from '../views/Modify';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -57,6 +61,8 @@ const StackScreen = () => {
               headerTitle: getFocusedRouteNameFromRoute(route),
             })}
           />
+          <Stack.Screen name="Modify" component={Modify} />
+          <Stack.Screen name="My Files" component={MyFiles} />
           <Stack.Screen name="Single" component={Single} />
         </>
       ) : (
